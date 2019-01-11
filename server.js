eac9c5540f97c07352a6a4a4a9fb12fd0a868868
Malik <Malik.Salim@cloudsolution-sa.com>
2019-01-11 17:24:20 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+var mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const app = express();
+var db;
+var CONFIG = require('./config.json');
+
+
+var userName = CONFIG.uname;
+var passWord = CONFIG.pword;
+var dbName = CONFIG.dbname;
+
+
+var URL = 'mongodb://' + userName + ':' + passWord + '@ds161190.mlab.com:61190/' + dbName;
+console.log(URL);
+
+app.use('/', express.static(__dirname));
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+mongoose.connect(URL);
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+
+app.listen(3000, () => {
+    console.log('listening on 3000');
+})
+
+app.get('/api/products', (req, res) => {
+    db.collection('products').find().toArray(function (err, results) {
+        console.log(results)
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify(results));
+    })
+})
+
+app.post('/api/product/add', (req, res) => {
+    db.collection('products').find({ id: req.body.id }).toArray(function (err, result) {
+        console.log(result);
+        if (result.length == 0) {
+            db.collection('products').save(req.body, (err, result) => {
+                if (err)
+                    return console.log(err);
+                console.log('saved to database');
+                res.end();
+            })
+        } else {
+            db.collection('products', function (err, collection) {
+                collection.remove({ id: req.body.id });
+                db.collection('products').save(req.body, (err, result) => {
+                    if (err)
+                        return console.log(err);
+                    console.log('exisiting ad updated in the database');
+                    res.end();
+                })
+            });
+        }
+    })
+})
